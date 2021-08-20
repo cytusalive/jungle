c98a866c935fb935610e6ac0934594c972a208b3
cytusalive <cytusomega@gmail.com>
2021-08-20 18:06:45 +0000

    simple admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"].to_s, password: ENV["PASSWORD"].to_s
   
   def show
+    @num_products = Product.count
+    @num_categories = Category.count
   end
 end
