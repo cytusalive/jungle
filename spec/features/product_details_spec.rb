d7fc9ef94b7190c4275a089f00aac51315b9eb86
cytusalive <cytusomega@gmail.com>
2021-08-25 22:41:03 -0700

    Feature Specs testing for product details page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to a Product Details page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They click the first product to see details" do
+    # ACT
+    visit root_path
+
+    first('.product').click_on "Details"
+
+    # DEBUG
+    save_and_open_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'article.product-detail'
+  end
+end
